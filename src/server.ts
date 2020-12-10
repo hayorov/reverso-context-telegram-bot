3ac91b8a718233de350981cc59de732b4af99f41
Mkhamat <myxa@xakep.org>
2020-12-10 18:59:17 +0300

    add connection to mongoDB

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
+import "dotenv/config"
 import express from "express"
-import axios from "axios"
-import dotenv from "dotenv"
 import bodyParser from "body-parser"
+import handleMessage from "./handleMessage"
+import mongoose from "mongoose"
 
-dotenv.config()
+mongoose
+  .connect(`${process.env.MONGO_URI}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => console.log(connection.connection.port))
 
 const app = express()
 
