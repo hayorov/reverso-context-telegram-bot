e1c66c3c5fce997d14d423af592e1a0646a75396
Mkhamat <myxa@xakep.org>
2020-12-10 18:59:37 +0300

    move functionality away

--- src/server.ts
+++ src/server.ts
@@ -19,28 +19,4 @@ app.listen(process.env.PORT, () => {
   console.log(`Server running on ${process.env.PORT}`)
 })
 
-app.post(`/${process.env.BOT_TOKEN}`, async (req, res) => {
-  res.status(200).end()
-
-  let translate = await axios
-    .post(
-      "https://api.reverso.net/translate/v1/translation",
-      {
-        from: "eng",
-        to: "rus",
-        input: req.body.message.text,
-        format: "text",
-        options: { origin: "contextweb", languageDetection: true },
-      },
-      { headers: { "Content-Type": "application/json" } }
-    )
-    .then((res) => {
-      return res.data.translation[0]
-    })
-    .catch((err) => console.error(err))
-
-  axios.post(
-    `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-    { chat_id: req.body.message.from.id, text: translate }
-  )
-})
+app.post(`/${process.env.BOT_TOKEN}`, handleMessage)
