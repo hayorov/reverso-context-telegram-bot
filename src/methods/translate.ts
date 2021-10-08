abacf5ebbcdcf97aa038c1a74855c617649172a2
mkhamat <myxa@xakep.org>
2021-10-08 19:11:08 +0300

    new request headers

--- src/methods/translate.ts
+++ src/methods/translate.ts
@@ -9,13 +9,31 @@ export default async function translate(
     .post(
       "https://api.reverso.net/translate/v1/translation",
       {
+        input: text,
         from: from,
         to: to,
-        input: text,
         format: "text",
-        options: { origin: "contextweb", languageDetection: true },
+        options: { origin: "reversodesktop", languageDetection: true },
       },
-      { headers: { "Content-Type": "application/json" } }
+      {
+        headers: {
+          'Connection': 'keep-alive',
+          'Pragma': 'no-cache',
+          'Cache-Control': 'no-cache',
+          'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+          'Accept': 'application/json, text/javascript, */*; q=0.01',
+          'Content-Type': 'application/json; charset=UTF-8',
+          'sec-ch-ua-mobile': '?0',
+          'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36',
+          'sec-ch-ua-platform': '"Linux"',
+          'Origin': 'https://www.reverso.net',
+          'Sec-Fetch-Site': 'same-site',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Dest': 'empty',
+          'Referer': 'https://www.reverso.net/',
+          'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8'
+        }
+      }
     )
     .then((res) => {
       return res.data.translation[0]
